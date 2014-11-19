62f5f81a5608ab60ed5c6aa4e3c32156f905c4d3
Danny Grove <danny@drgrovellc.com>
2014-11-19 08:22:26 -0800

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name securechatApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the securechatApp
+ */
+angular.module('securechatApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
